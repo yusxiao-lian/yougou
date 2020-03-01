3adcb5de8c49386e6a5184d5a7fa0db27cc21c1c
lianyuxiao <1049146413@qq.com>
2020-03-02 01:15:52 +0800

    完成分类页

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,34 @@
-// pages/category/index.js
+import request from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 数据信息
+    menusData: [],
+    // 当前页
+    currentpage: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    request({
+      url: "/categories"
+    }).then((res) => {
+      this.setData({
+        menusData: res.data.message
+      })
+      console.log(res)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击切换菜单
+  handlechange (e) {
+    let index = e.currentTarget.dataset.index
+    this.setData({
+      currentpage: index
+    })
   }
 })
