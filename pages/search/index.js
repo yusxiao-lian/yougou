ae80843edc0e1846a37c3b7a5d178a995cb0c604
lianyuxiao <1049146413@qq.com>
2020-03-05 22:34:51 +0800

    完成搜索页

--- pages/search/index.js
+++ pages/search/index.js
@@ -9,17 +9,25 @@ Page({
     // 搜索输入框的值
     searchValue: '',
     // 搜索商品列表
-    goodsList: []
+    goodsList: [],
+    // 请求状态
+    hasrequest: false,
+    // 最后搜索值
+    lastrequest: '',
+    // 历史搜索记录
+    historylist: []
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onLoad(){
+    let historylist = wx.getStorageSync('historylist') || []
+    this.setData({
+      historylist
+    })
   },
+
   // 输入框数据发生改变
   handlechange(e){
+    
     // console.log(e)
     // 获取输入值并保存
     let {value} = e.detail
@@ -27,17 +35,36 @@ Page({
     this.setData({
       searchValue: value
     })
-    console.log(Boolean(value))
+
+    // 搜索值为空再清空搜索数据
+    if (value == '') {
+      this.setData({
+        goodsList: []
+      })
+    }
+    // console.log(Boolean(value))
     // 如果输入值为空则不发送请求
     if (!value) {
       return;
     }
+
     // 发送搜索请求
     this.getProducts()
   },
 
   // 输入框发生改变时搜索数据
   getProducts () {
+    
+    // 判断上次请求是否完成
+    if (this.data.hasrequest !== false) {
+      return;
+    }
+    console.log(this.data.searchValue)
+    this.setData({
+      hasrequest: true,
+      // 记录最后一次搜索值
+      lastrequest: this.data.searchValue
+    })
     request({
       url: "/goods/qsearch",
       data: {
@@ -46,9 +73,45 @@ Page({
     }).then((res) => {
       console.log(res)
       this.setData({
-        goodsList: res.data.message
+        goodsList: res.data.message,
+        hasrequest: false
       })
+      // 判断是否进行了最后一次搜索,如果不是则再发送请求
+      if (this.data.lastrequest !== this.data.searchValue) {
+        this.getProducts()
+      }
+    })
+  },
+  // 点击取消按钮
+  handleCancle () {
+    this.setData({
+      searchValue: '',
+      goodsList: []
+    })
+  },
+  // 按下enter键时
+  handleEnter (e) {
+    // console.log(e.detail)
+    // 保存搜索记录
+    let historylist = wx.getStorageSync('historylist') || []
+    historylist.unshift(e.detail.value)
+    // 数组去重
+    let newdata = [...new Set(historylist)]
+    if (newdata.length >10) {
+      newdata.pop()
+    }
+    wx.setStorageSync('historylist', newdata)
+    console.log(newdata)
+    wx.redirectTo({
+      url: '/pages/goods_list/index?keyword=' + e.detail.value
+    })
+  },
+
+  // 清空历史记录
+  handleDel () {
+    wx.setStorageSync('historylist', [])
+    this.setData({
+      historylist: []
     })
   }
-
 })
