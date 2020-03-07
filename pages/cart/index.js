93ec9caeaf84054eaacf8ffe3f99b83d11c8ac5f
lianyuxiao <1049146413@qq.com>
2020-03-08 01:45:45 +0800

    购物车

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,72 @@
 // pages/cart/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // 收货信息
+        address: {},
+        // 商品列表
+        goodsList: [],
+        // 总价
+        totalPrice: 0
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        // 获取收货地址
+        this.setData({
+            address: wx.getStorageSync("address") || {}
+        })
+        // console.log(this.data.address)
+        
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        // 获取商品列表
+        let goods = wx.getStorageSync("goodsList") || []
+        this.setData({
+            goodsList: goods
+        })
+        console.log(this.data.goodsList)
+        // 计算总价
+        this.getTotalPrice()
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    // 获取收货地址
+    getAddress() {
+        wx.chooseAddress({
+            success: (res) => {
+                this.setData({
+                    address: {
+                        username: res.userName,
+                        useraddress: res.provinceName + res.cityName + res.countyName + res.detailInfo,
+                        tel: res.telNumber
+                    }
+                })
 
-  },
+                // 保存本地
+                wx.setStorageSync('address', this.data.address);
+            }
+        })
+    },
+    // 计算总价格
+    getTotalPrice () {
+        let price = 0
+        this.data.goodsList.forEach(value => {
+            price += value.number*value.goods_price 
+        })
+        // console.log(price)
+        this.setData({
+            totalPrice: price
+        })
+    }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -2,63 +2,100 @@
 import request from '../../utils/request.js'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    // 商品信息详情
-    productDetail: '',
-    // tab当前点击状态
-    currentIndex: 0,
-    // 轮播图路径
-    imgUrl: []
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // 商品信息详情
+        productDetail: '',
+        // tab当前点击状态
+        currentIndex: 0,
+        // 轮播图路径
+        imgUrl: []
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    // console.log(options)
-    let id = options.id
-    request({
-      url: "/goods/detail",
-      data: {
-        goods_id: id
-      }
-    }).then((res) => {
-      console.log(res)
-      // 存储图片路径
-      let img = res.data.message.pics
-      let imgUrl = img.map((item) => {
-        return item.pics_big
-      })
-      this.setData({
-        productDetail: res.data.message,
-        imgUrl
-      })
-      
-    })
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        // console.log(options)
+        let id = options.id
+        request({
+            url: "/goods/detail",
+            data: {
+                goods_id: id
+            }
+        }).then((res) => {
+            console.log(res)
+            // 存储图片路径
+            let img = res.data.message.pics
+            let imgUrl = img.map((item) => {
+                return item.pics_big
+            })
+            this.setData({
+                productDetail: res.data.message,
+                imgUrl
+            })
 
-  // tab栏切换
-  handleSelect(e){
-    // console.log(e.currentTarget.dataset)
-    this.setData({
-      currentIndex: e.currentTarget.dataset.index
-    })
-  },
-  // 图片预览
-  previewImg(e){
-    let index = e.currentTarget.dataset.index;
-    wx.previewImage({
-      current: this.data.imgUrl[index], // 当前显示图片的http链接
-      urls: this.data.imgUrl // 需要预览的图片http链接列表
-    })
-  },
-  // 跳转到购物车页
-  goToCart() {
-    wx.switchTab({
-      url: '/pages/cart/index',
-    })
-  }
+        })
+    },
+
+    // tab栏切换
+    handleSelect(e) {
+        // console.log(e.currentTarget.dataset)
+        this.setData({
+            currentIndex: e.currentTarget.dataset.index
+        })
+    },
+    // 图片预览
+    previewImg(e) {
+        let index = e.currentTarget.dataset.index;
+        wx.previewImage({
+            current: this.data.imgUrl[index], // 当前显示图片的http链接
+            urls: this.data.imgUrl // 需要预览的图片http链接列表
+        })
+    },
+    // 跳转到购物车页
+    goToCart() {
+        wx.switchTab({
+            url: '/pages/cart/index',
+        })
+    },
+    // 加入购物车
+    addCart() {
+        // 获取本地存储商品
+        let goodsList = wx.getStorageSync("goodsList") || []
+
+        // 判断商品是否已经存在
+        let exist = goodsList.some(item => {
+            let isExist = item.goods_id == this.data.productDetail.goods_id
+            // 如果已经存在则商品数量加一
+            if (isExist) {
+                item.number += 1
+                wx.showToast({
+                    title: '数量+1',
+                    icon: 'success'
+                })
+            }
+            return isExist
+        })
+
+        // 如果不存在则将商品添加
+        if (!exist) {
+            goodsList.unshift({
+                goods_id: this.data.productDetail.goods_id,
+                goods_price: this.data.productDetail.goods_price,
+                goods_name: this.data.productDetail.goods_name,
+                goods_small_logo: this.data.productDetail.goods_small_logo,
+                number: 1
+            })
+            wx.showToast({
+                title: '加入成功',
+                icon: 'success'
+            })
+        }
+       
+        // 重新写入本地存储
+        wx.setStorageSync('goodsList', goodsList)
+    }
 })
